87baf89331be37b86e4374e571897a02c4478b36
MichellMurallas <michellsms893@gmail.com>
2023-02-10 10:47:34 -0500

    meses del segundo semestre del año

---
+++ Calculadora.js
@@ -0,0 +1,11 @@
+
+let meses = [
+  {
+    julio: "septimo mes del año",
+    agosto: "octavo mes del año",
+    septiembre: "noveno mes del año",
+    octubre: "decimo mes del año",
+    noviembre: "onceavo mes del año",
+    diciembre: "doceavo mes del año"
+  }
+]
