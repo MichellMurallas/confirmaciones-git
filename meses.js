167585ef5bc30f1810bec411dba0b0c5dbe30de9
Miguel Angel <96792398+MichellMurallas@users.noreply.github.com>
2023-02-10 10:10:11 -0500

    creacion del primer semestre del año

---
+++ meses.js
@@ -0,0 +1,10 @@
+let meses = [
+  {
+    enero: "primer mes del año",
+    febrero: "segundo mes del año",
+    marzo: "tercer mes del año",
+    abril: "cuarto mes del año",
+    mayo: "quinto mes del año",
+    junio: "sexto mes del año"
+  }
+]
