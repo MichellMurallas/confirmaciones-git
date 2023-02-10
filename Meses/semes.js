e53e06dcd24f9d2be2a4446b07b534883de0a7d4
MichellMurallas <michellsms893@gmail.com>
2023-02-10 13:18:55 -0500

    se unieron todos los meses con su informacion en dos variables lamadas semestres

--- Meses/semes.js
+++ Meses/semes.js
@@ -1,11 +1,22 @@
 
 let firstSemester = [
   {
-    enero:   "",
-    febrero: "",
-    marzo:   "",
-    abril:   "",
-    mayo:    "",
-    junio:   ""
+    enero:   "primer mes del año",
+    febrero: "segundo mes del año",
+    marzo:   "tercer mes del año",
+    abril:   "cuarto mes del año",
+    mayo:    "quinto mes del año",
+    junio:   "sexto mes del año"
+  },
+];
+
+let secondSemester = [
+  {
+    julio:      "septimo mes del año",
+    agosto:     "octavo mes del año",
+    septiembre: "noveno mes del año",
+    octubre:    "decimo mes del año",
+    noviembre:  "onceavo mes del año",
+    diciembre:  "doceavo mes del año"
   }
 ];
