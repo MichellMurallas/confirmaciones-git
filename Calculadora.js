b43cf11796ecad7f38cddfc693ff4d0729428124
MichellMurallas <michellsms893@gmail.com>
2023-02-10 09:57:44 -0500

    adicionn de Calculadora

---
+++ Calculadora.js
@@ -0,0 +1,13 @@
+
+
+let semana = [
+  {
+    domingo:   "primer dia de la semana",
+    lunes:     "segundo dia de lla semana",
+    martes:    "tercerdia de la semana",
+    miercoles: "cuarto dia de la semana",
+    jueves:    "quinto dia de la semana",
+    viernes:   "sexto dia de la semana",
+    sabado:    "septimo dia de la semana" 
+  }
+]
