70e930af9fb2270bf1357fcf7928a3a8fd7c16b7
MichellMurallas <michellsms893@gmail.com>
2023-02-15 23:44:47 -0500

    ok dias de la semana

---
+++ Semana/Semana.js
@@ -0,0 +1,12 @@
+
+const semana = [
+  {
+    domingo:   "Primer dia de la semana",
+    lunes:     "Segundo dia de la semana",
+    martes:    "Tercer dia de la semana",
+    miercoles: "Cuarto dia de la semana",
+    jueves:    "Quinto dia de la semana",
+    viernes:   "Sexto dia de la semana",
+    sabado:    "septimo dia de la semana"
+  }
+]
