72357f4d231072c376cbac5a3a3b27d606fe1649
MichellMurallas <michellsms893@gmail.com>
2023-03-03 17:21:09 -0500

    personas

---
+++ Personas/Personas.js
@@ -0,0 +1,31 @@
+
+personas = [
+  {
+    nombres: "Pedro",
+    apellidos: "Ramirez",
+    estatura: "195",
+    telefono: "7430283",
+    profesion: "Ingeniero"
+  },
+  {
+    nombres: "Carlos",
+    apellidos: "Cardona Pardo",
+    estatura: "172",
+    telefono: "7223301",
+    profesion: "Ingeniero"
+  },
+  {
+    nombres: "Julian",
+    apellidos: "Mendoza Perez",
+    estatura: "184",
+    telefono: "7230104",
+    profesion: "Medico"
+  },
+  {
+    nombres: "Luis Miguel",
+    apellidos: "Impuesto Gaston",
+    estatura: "187",
+    telefono: "7250104",
+    profesion: "Abogado"
+  },
+]
